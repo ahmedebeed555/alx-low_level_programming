225a7103b0778662dc81d64dbebbc572cc973439
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-15 15:37:53 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	printf("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

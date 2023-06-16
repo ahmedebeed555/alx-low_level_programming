afb23544f6f8b5782a2450d7c613f4be9ede0a62
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-16 13:59:27 -0700

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 int digit1, digit2, digit3;
 
 for (digit1 = 0; digit1 < 8; digit1++)
- {
+{
 for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
 for (digit3 = digit2 + 1; digit3 < 10; digit3++)
@@ -24,7 +24,6 @@ putchar((digit3 % 10) + '0');
 
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
-				
 putchar(',');
 putchar(' ');
 }

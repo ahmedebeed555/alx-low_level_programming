3f9a42c0899f0264eb751da04b88cb4f871260e1
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-16 12:25:19 -0700

    task 9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,14 @@
 int main(void)
 {
 	int n;
-
-for (n = 0; n <= 15; n++)
+	char c;
+for (n = 0; n <= 10; n++)
 {
-putchar(n + '0');
+putchar((n % 10)+ '0');
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
 }
 putchar('\n');
 return (0);

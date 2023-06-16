d8fd0ccfd40b09d3e555d72cc604df00f1f250c8
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-16 12:46:05 -0700

    task 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+if (n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

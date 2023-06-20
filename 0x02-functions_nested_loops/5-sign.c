8e984815fcbf4a0bece89ba1e910158938e7a497
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-20 09:49:39 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,14 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
- else if (n < 0)
+else if (n < 0)
 {
-  _putchar(45);
+_putchar(45);
 return (-1);
 }
 else
 {
-  _putchar(48);
+_putchar(48);
 return (0);
 }
 }

83a7f3b0f6b8add1bb62e7e3f2e0c1a376978f29
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-20 09:48:00 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * _isalpha - Check if alphabetic character
+  * n: The number to check
+  *
+  * Return: 1 for n > 0
+  *
+  * Return: 0 of input 0
+  *
+  * Return: -1 for n < 0
+  */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+ else if (n < 0)
+{
+  _putchar(45);
+return (-1);
+}
+else
+{
+  _putchar(48);
+return (0);
+}
+}

3fdbfb76ed0e8a9aa95406bbe85c8a18e99333c7
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-21 04:08:07 -0700

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}

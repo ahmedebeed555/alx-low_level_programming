fd1e1c23c3195d05185542bdb0a747f5ad132702
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-26 00:22:07 -0700

    task  1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int() - function to swap integers
+ *
+ * @a : first pointer
+ *
+ * @b : second pointer
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int c;
+c = *a;
+*a = *b;
+*b = c;
+
+}

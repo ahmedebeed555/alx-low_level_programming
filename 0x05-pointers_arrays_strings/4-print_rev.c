080c23e807e97996f462bc259c72ade48a734cd8
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-26 01:25:39 -0700

    task  4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse
+ *
+ * @s : pointer to the string
+ *
+ */
+void print_rev(char *s)
+{
+int len, j;
+while(s[len] != '\0')
+{
+len++;
+}
+for(j = len - 1; j >=0 ; j-- )
+{
+_putchar(s[j]);
+}
+_putchar("\n");
+}

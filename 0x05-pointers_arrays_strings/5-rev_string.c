f75c4945f34c1972b1184362a142cac4d42b7754
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-26 02:35:17 -0700

    task  5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses the string
+ *
+ * @s : string 
+ *
+ * Return: string recersed
+ *
+ *
+ */
+void rev_string(char *s);
+{
+int len = 0 , j;
+char t[len];
+while(s[len] != '\0')
+{
+len++
+}
+for(j = len -1; j >= 0; j--)
+{
+t[len - j] = s[j];
+}
+return(t[len]);
+}

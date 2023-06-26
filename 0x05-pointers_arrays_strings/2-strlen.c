b3c45f819f63434ced94e8ac6188396589efed3b
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-06-26 00:48:29 -0700

    task  2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}

4e3a3173aa9419aa4fd98af70d1b754de50262f2
ahmedebeed555 <ahmedebeed555@yahoo.com>
2023-08-07 12:38:14 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w =n write(o, text_content, len);
+	w =n write(opn, text_content, len);
 
 	if (opn == -1 || w == -1)
 		return (-1);
